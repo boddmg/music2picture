e9e3b5ac342dde7ed384d1f3144b3f92aaeee308
boddmg <boddmg@gmail.com>
2013-06-18 08:44:05 +0800

    把选择音乐文件的对话框的mp3的过滤器去掉了
    Signed-off-by: boddmg <boddmg@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 #Music to Picture
 ##Idea from wendy
 这是一个把一首音乐转化成一幅画的开源项目
+2013/6/20 V0.1发布 该版本只能读取wav格式的音乐
--- music2picture.py
+++ music2picture.py
@@ -89,7 +89,7 @@ def addColorElement(flame,index,hue):
 
 def getMusicFilePath():
     dialog = wx.FileDialog(None, message="选择要转换的音乐", defaultDir="", 
-        defaultFile="", wildcard="Music (*.wav;*.mp3)|*.wav;*.mp3|All files (*.*)|*.*", style=0, 
+        defaultFile="", wildcard="Music (*.wav)|*.wav|All files (*.*)|*.*", style=0, 
         pos=wx.DefaultPosition)
     if dialog.ShowModal() == wx.ID_OK:
         path=dialog.GetPath()
