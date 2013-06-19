117711edbca438e5ecee29fd4acc87d63cb1f7ad
boddmg <boddmg@gmail.com>
2013-06-19 23:39:43 +0800

    目前只有颜色是从音乐的音域特征过来的。
    Signed-off-by: boddmg <boddmg@gmail.com>

--- a.py
+++ a.py
@@ -1,8 +1,9 @@
 # -*- coding: cp936 -*-
-import wx,pygame
+import pymedia,os,wx
+
 def getMusicFilePath():
     dialog = wx.FileDialog(None, message="选择要转换的音乐", defaultDir="", 
-        defaultFile="", wildcard="Music (*.wav;*.mp3)|*.wav;*.mp3|All files (*.*)|*.*", style=0, 
+        defaultFile="", wildcard="Music (*.wav;*.wma;*.mp2;*.mp3;*.ac3;*.aac;*.flac;*.ogg)|*.wav;*.wma;*.mp2;*.mp3;*.ac3;*.aac;*.flac;*.ogg", style=0, 
         pos=wx.DefaultPosition)
     if dialog.ShowModal() == wx.ID_OK:
         path=dialog.GetPath()
@@ -10,3 +11,8 @@ def getMusicFilePath():
         path=False
     dialog.Destroy()
     return path
+
+wxapp=wx.App()
+path=getMusicFilePath()
+fileType=os.path.splitext(path)[1][1:]
+musicData=open(path,'rb').read()
